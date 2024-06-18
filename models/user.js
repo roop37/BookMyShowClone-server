fe725c1b8baaf2f37d56d2e7b37c49853c3fea07
Debjeet Banerjee <debjeet.mailme@gmail.com>
2024-06-18 23:06:50 +0530

    second

--- models/user.js
+++ models/user.js
@@ -1,3 +1,32 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required:true
+  },
+  email:{
+    type:String,
+    require:true
+  },
+  phoneNumber:{
+    type:Number,
+    required:true
+  },
+  booking: [{
+    type: Schema.Types.ObjectId,
+    ref: 'event'
+  }],
+  currentCity:{
+    type:String,
+    required:true
+  },
+  currentState:{
+    type:String,
+    required:true
+  }
+});
+
+const User=mongoose.model("user",userSchema);
+
+module.exports=User;
